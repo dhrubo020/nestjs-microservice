5659f01354a48030593cb833783e8fc118d71f5f
taifdroid <taifbakshi.dev@gmail.com>
2024-03-02 15:06:17 +0600

    updated

--- task-service/config/index.ts
+++ task-service/config/index.ts
@@ -1,8 +1,9 @@
 export const localIp = '54.175.99.111';
 const monitoringIp = '54.145.6.11';
 
+const rabbitMq = process.env.RABBIT_MQ
 
 export const lokiHostUrl = `http://${monitoringIp}:3100` || '';
 
-export const amqpUrl = `amqp://${localIp}:5672` || '';
+export const amqpUrl = rabbitMq || '';
 export const jaegerEndpoint = `http://${localIp}:14268/api/traces`;
