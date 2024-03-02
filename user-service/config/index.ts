7119e393f2ba3ad8189b82e5abc41ed666789c60
taifdroid <taifbakshi.dev@gmail.com>
2024-03-02 14:54:56 +0600

    added ec2 instance public ip in config

--- user-service/config/index.ts
+++ user-service/config/index.ts
@@ -1,5 +1,8 @@
-// export const localIp = '172.18.105.191';
-export const localIp = 'http://54.175.99.111';
+export const localIp = '54.175.99.111';
+const monitoringIp = '54.145.6.11';
+
+
+export const lokiHostUrl = `http://${monitoringIp}:3100` || '';
+
 export const amqpUrl = `amqp://${localIp}:5672` || '';
-export const lokiHostUrl = `http://${localIp}:3100` || '';
 export const jaegerEndpoint = `http://${localIp}:14268/api/traces`;
